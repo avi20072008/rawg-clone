deafb75c407b995c8d0445a88f2081f1d2b0f76f
Avinash <meetjasper@gmail.com>
2023-05-12 15:59:57 -0500

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
+import noImage from '../assets/no-image-placeholder.webp';
 
 function GetCroppedImageUrl(url: string){
-    console.log('url' + url);
-    if(url === null) return '';
+    //If image is null
+    if(!url) return noImage;
+    
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     const newURL = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
